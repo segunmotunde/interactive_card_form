cb51469eb6a3920e5956c103f01baaf405bdbc41
segunmotunde <sogunledun@gmail.com>
2023-03-16 22:24:21 +0100

    set up the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Main from './Components/Main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Welcome to Interactive Form
+      <Main/>
     </div>
   )
 }
---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './Card'
+import Form from './Form'
+
+
+const Main = () => {
+  return (
+    <div>
+        <div>
+            <Card/>
+            <Form/>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Main
