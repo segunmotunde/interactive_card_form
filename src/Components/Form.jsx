ac537ea2307e2f6fe6f0d4331b8784e80214b8a0
Odili1 <ezedigwegerald1@gmail.com>
2023-03-17 03:59:24 +0100

    initial form commit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div>
+      <form>
+        <div>
+          <label for="name">CARDHOLDER NAME</label>
+          <input type={"text"} className='name' id='name' placeholder='e.g. Jane Appleaseed'></input>
+        </div>
+        <div>
+          <label for="number">CARD NUMBER</label>
+          <input type={"number"} className='number' id='number' placeholder='e.g. 1234 5678 9123 0000'></input>
+        </div>
+        <div>
+          <label for="expiry">EXP. DATE (MM/YY)</label>
+          <input type={"number"} className='expiry' id='expiryMM' placeholder='MM'></input>
+          <input type={"number"} className='expiry' id='expiryYY' placeholder='YY'></input>
+        </div>
+        <div>
+          <label for="cvc">CVC</label>
+          <input className='cvc' id='cvc' placeholder='e.g. 123'></input>
+        </div>
+        <div>
+          <button type='submit'>Confirm</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
