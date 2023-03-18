8077fbd94bd0ab470532109247e3e49f8a0192d1
segunmotunde <sogunledun@gmail.com>
2023-03-18 22:28:59 +0100

    fix mobile responsiveness

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,18 +3,21 @@ import card from './card.module.css'
 
 const Card = () => {
   return (
-    <div className={card.Container}>
-        <div className={card.cardFront}>
-            <img src='./images/bg-card-front.png' alt="cfngjgn" />
-            <p>0000 0000 0000 0000</p>
-            <div className={card.nameContainer}>
-                <span>JANE APPLEASEED</span>
-                <span>00/00</span>
-            </div>
-            </div>
-        <div className={card.cardBack}><img src='./images/bg-card-back.png' alt="cfngjgn" />
-          <p>000</p>
-        </div>
+    <div className={card.body}>
+      <div className={card.Container}>
+          <div className={card.cardFront}>
+              <img src='./images/bg-card-front.png' alt="cfngjgn" />
+              <p>0000 0000 0000 0000</p>
+              <div className={card.nameContainer}>
+                  <span>JANE APPLEASEED</span>
+                  <span>00/00</span>
+              </div>
+              </div>
+          <div className={card.cardBack}>
+            <img src='./images/bg-card-back.png' alt="cfngjgn" />
+            <p>000</p>
+          </div>
+      </div>
     </div>
   )
 }
