315158ddedfad5ce39dbc44a4fe80cdd87e18a02
Odili1 <ezedigwegerald1@gmail.com>
2023-03-22 02:11:24 +0100

    styled thankyou page

--- src/Components/ThankPage.jsx
+++ src/Components/ThankPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import thanks from './thankyouPage.module.css'
 
 const ThankPage = () => {
   return (
-    <div>
-      <div></div>
-      <h2>THANK YOU</h2>
+    <div className={thanks.container}>
+      <div><img src='../p'/></div>
+      <h2>THANK YOU!</h2>
       <p>We've added your card details</p>
       <button>Continue</button>
     </div>
